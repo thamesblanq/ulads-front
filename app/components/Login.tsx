b78626a1b7dbf0c7b78e92c713b6018dfe3f1701
blanq31 <obiwulugodswill@gmail.com>
2026-06-14 00:24:34 +0100

    what the hell

--- app/components/Login.tsx
+++ app/components/Login.tsx
@@ -41,15 +41,12 @@ export default function LoginPage() {
 		setIsLoading(true);
 
 		try {
-			const response = await fetch(
-				`${process.env.NEXT_PUBLIC_API_URL}/auth/login`,
-				{
-					method: "POST",
-					headers: { "Content-Type": "application/json" },
-					body: JSON.stringify({ email, password }),
-					credentials: "include", // Essential for HttpOnly cookies,
-				},
-			);
+			const response = await fetch("api/auth/login", {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify({ email, password }),
+				credentials: "include", // Essential for HttpOnly cookies,
+			});
 
 			if (!response.ok) {
 				throw new Error("Invalid email or password.");
