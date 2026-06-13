75eacfb9a563732417f96aa17202cdf89f8418e9
blanq31 <obiwulugodswill@gmail.com>
2026-06-13 23:53:04 +0100

    fix: im tired

--- app/components/Login.tsx
+++ app/components/Login.tsx
@@ -49,7 +49,7 @@ export default function LoginPage() {
 					method: "POST",
 					headers: { "Content-Type": "application/json" },
 					body: JSON.stringify({ email, password }),
-					credentials: "include", // Essential for HttpOnly cookies
+					credentials: "include", // Essential for HttpOnly cookies,
 				},
 			);
 
