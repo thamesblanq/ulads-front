de8763ae99a03b40674d27e4c143507c8723c261
blanq31 <obiwulugodswill@gmail.com>
2026-06-19 13:46:09 +0100

    fix: wrong import on admindashboardskeleton

--- app/dashboard/admin/loading.tsx
+++ app/dashboard/admin/loading.tsx
@@ -1,5 +1,5 @@
 // app/dashboard/admin/loading.tsx
-import { AdminDashboardSkeleton } from "../../components/AdminDashboardSkeleton";
+import { AdminDashboardSkeleton } from "../../components/skeletons/AdminDashboardSkeleton";
 
 export default function Loading() {
 	return <AdminDashboardSkeleton />;
